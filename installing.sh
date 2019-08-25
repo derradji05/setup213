f1ae5115918854a0632f3004bb39bd803cd05c26
Derradji <50505545+derradji05@users.noreply.github.com>
2019-08-26 00:14:21 +0100

    Add files via upload

---
+++ installing.sh
@@ -0,0 +1,128 @@
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-->            '$g']*'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'----->         '$g']*'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------>        '$g']*'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------->      '$g']*'
+rm nn.txt
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+sleep 0.6
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'----------->   '$g']*'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+sleep 0.1
+clear
+echo -e 'Wa'$p'/'
+clear
+echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+sleep 0.1
+clear
+figlet -f big "D-Dragon"
+sleep 1
+echo "     "
+echo -e "     welcome in the Basic Terminal"
+sleep 2
+echo "     "
+echo "     "
+echo "     "
+echo "     "
+apt update -y
+apt upgrade -y
+pkg install update -y
+pkg install upgrade -y
+echo "     "
+echo "     "
+echo -e "   CLICK ALLOW "
+echo "     "
+echo "     "
+termux-setup-storage
+apt-get update -y
+apt-get upgrade -y
+apt install php -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install nano -y
+apt install hydra -y
+apt install cmatrix -y
+pkg install figlet -y
+pkg install wget -y
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install ruby -y
+pkg install help -y
+gem install lolcat -y
+pkg install curl -y
+pkg install unzip -y
+pkg install openssh -y
+pkg install uzip -y
+pkg install net-tools -y
+pkg install unrar -y
+pkg install clang -y
+pip2 install wget -y
+apt update -y
+
+rm -rf $HOME/setup
+git clone https://github.com/derradji05/D_Dragon.git
+
+echo -e "   Password Of Tool : 213"
